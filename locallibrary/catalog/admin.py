6bb04eea9ea277aeca1bd5f2d27d63bb2b3976af
Mikhail Dyoshin <mikhail.dyoshin@gmail.com>
2022-10-19 12:56:01 +0300

    Created ModelAdmin classes

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -4,6 +4,20 @@ from .models import Genre, Language, Book, BookInstance, Author
 
 admin.site.register(Genre)
 admin.site.register(Language)
-admin.site.register(Book)
-admin.site.register(BookInstance)
-admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+# admin.site.register(Author)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
