e091fe231b9f62daa3c40887dc99dc133047c8e0
Mikhail Dyoshin <mikhail.dyoshin@gmail.com>
2022-10-19 16:23:07 +0300

    Created index page template, url mapping for the page and index view

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+    """View function for home page of the site"""
+
+    page_name = "Home Page"
+
+    num_books = Book.objects.all().count()
+
+    num_instances = BookInstance.objects.all().count()
+
+    instances_available = BookInstance.objects.filter(status__exact='a')
+    num_instances_available = instances_available.count()
+
+    num_novels = Book.objects.filter(genre__name__icontains='novel').count()
+
+    num_authors = Author.objects.all().count()
+
+    context = {
+        'page_name': page_name,
+        'num_books': num_books,
+        'num_novels': num_novels,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'catalog/index.html', context=context)
