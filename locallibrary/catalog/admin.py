c764356fed3445fc47648ace28d1f239dde6fcfe
Mikhail Dyoshin <mikhail.dyoshin@gmail.com>
2022-10-19 12:50:07 +0300

    Registered my models

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Genre, Language, Book, BookInstance, Author
 
-# Register your models here.
+
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Book)
+admin.site.register(BookInstance)
+admin.site.register(Author)
