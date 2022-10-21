e5d14af6ce65bb97a1b40427918389e8f1d48c85
Mikhail Dyoshin <mikhail.dyoshin@gmail.com>
2022-10-21 15:46:32 +0300

    Added getting-visit-counts option to the index view.
    Now on the home page one can see how many times one visited the home page.

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -19,6 +19,9 @@ def index(request):
 
     num_authors = Author.objects.all().count()
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'page_name': page_name,
         'num_books': num_books,
@@ -26,6 +29,7 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     return render(request, 'catalog/index.html', context=context)
